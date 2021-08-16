a50f9553299f49b358ede9bab1fbdb05fcd08bb3
Uriel Hurtado <52264906+ualonso85@users.noreply.github.com>
2021-08-15 20:01:17 -0500

    Update bowtie2_map_pipeline.sh

--- bowtie2_map_pipeline.sh
+++ bowtie2_map_pipeline.sh
@@ -13,7 +13,7 @@ echo "-------------------------------------------------------------------------"
 
 for i in `ls *.fastq.gz | cut -d "_" -f 1` ; #this line is important to run PE data (R1 and R2)
 
-    do ~software/bowtie2-2.4.1-linux-x86_64/bowtie2 -p 20 -x ~jenny/Eflux_Pumps_Raw_data_Genewiz/mapping/MTB_H37Rv_Gref$
+    do ~software/bowtie2-2.4.1-linux-x86_64/bowtie2 -p 20 -x ~jenny/Eflux_Pumps_Raw_data_Genewiz/mapping/MTB_H37Rv_Gref_NC_000962.3_inx -1 $i\_R1_paired2.fastq.gz -2 $i\_R2_paired2.fastq.gz -S $Mapping/$i.sam ;
 
         samtools view -bS -@ 16 $Mapping/$i.sam > $Mapping/$i.bam
 
